4e9205e6b0781195c4ca18398c607d78750cc40b
Andrea Chello <chelloandrea@gmail.com>
2021-09-01 23:27:48 +0100

    refactored UI Buttons into its own file, added music from the model.js and activated toggle sound and music on inside options menu, bg-music will automatically start once game is started

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import 'phaser';
+import Model from './Model';
 import config from './Config/config';
 import GameScene from './Scenes/GameScene';
 import BootScene from './Scenes/BootScene';
@@ -15,6 +16,8 @@ var home = document.getElementById("home")
 class Game extends Phaser.Game {
   constructor () {
     super(config);
+    const model = new Model();
+    this.globals = { model };
     this.scene.add('Boot', BootScene);
     this.scene.add('Preloader', PreloaderScene);
     this.scene.add('Title', TitleScene);
